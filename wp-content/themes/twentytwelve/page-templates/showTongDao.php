ee7195c90908caac868d8477bbc37cd844cc966a
csboyty <csboyty@gmail.com>
2014-06-10 08:43:51 +0800

    修改了通道展示页

--- wp-content/themes/twentytwelve/page-templates/showTongDao.php
+++ wp-content/themes/twentytwelve/page-templates/showTongDao.php
@@ -22,11 +22,19 @@
     <title><?php  wp_title("|",true,"right"); ?></title>
     <link href="<?php echo get_template_directory_uri(); ?>/css/frontend/src/common.css" rel="stylesheet" type="text/css">
     <style type="text/css">
+        a{
+            color: #999;
+        }
+        a:hover{
+            color:#333;
+        }
         .title{
             text-align: center;
         }
         video{
             max-width: 100%;
+            width:85%;
+            margin:auto;
             height: auto;
             display: block;
         }
@@ -34,6 +42,12 @@
             width: auto;
             margin: 20px auto;
             text-align: center;
+        }
+        .links{
+            text-align: center;
+        }
+        .links .linkImage{
+            vertical-align: middle;
         }
     </style>
     <script src="<?php echo get_template_directory_uri(); ?>/js/frontend/src/googleAnalytics.js"></script>
@@ -46,6 +60,10 @@
     <video src="http://s40yes.bdcdn.duapp.com/dongjin1.mp4" autoplay="autoplay" controls="controls"></video>
     <div class="moreInfo">For more information, please follow our official Weibo
         <br><a href="http://weibo.com/newchannel2011" target="_blank">@新通道社会创新</a>
+        <div class="links">
+            <img class="linkImage" src="<?php echo get_template_directory_uri(); ?>/images/frontend/app/erweima.jpg">
+            <a href="http://www.ljydesign.it/">http://www.ljydesign.it</a>
+        </div>
     </div>
 </section>
 
